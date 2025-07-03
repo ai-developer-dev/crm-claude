fea56e1085942f6b9a87ab900b4cfd88c3b77c13
Doug Allen <dkallen1020@gmail.com>
2025-07-03 15:58:37 -0500

    Fix TypeScript error for PORT   parsing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -62,7 +62,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT || '5000', 10);
 
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
